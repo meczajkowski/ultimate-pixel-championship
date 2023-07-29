5d5cd3147520a9bc98c08210d27e91e1a13471ae
Michal <michal.erik.czajkowski@gmail.com>
2023-07-30 00:55:38 +0200

    feat: create welcome screen ui - create button primary

--- js/screens/welcome-screen.js
+++ js/screens/welcome-screen.js
@@ -1,5 +1,12 @@
 export const welcomeScreen = () => {
    const welcomeStepWrapper = document.createElement('div');
+   const welcomeStepButtonPrimary = document.createElement('button');
+
+   welcomeStepButtonPrimary.classList.add('button', 'button--primary');
+   welcomeStepButtonPrimary.innerText = 'Choose';
+
    welcomeStepWrapper.innerText = 'Welcome screen';
+   welcomeStepWrapper.append(welcomeStepButtonPrimary);
+
    return welcomeStepWrapper;
 };
