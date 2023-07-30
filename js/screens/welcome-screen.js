b88efc41f0d9ce4492831092887f8d759ded4620
Michal <michal.erik.czajkowski@gmail.com>
2023-07-30 22:28:56 +0200

    feat: create welcome screen ui - create welcome heading

--- js/screens/welcome-screen.js
+++ js/screens/welcome-screen.js
@@ -9,7 +9,28 @@ export const welcomeScreen = () => {
       return element;
    };
 
+   const welcomeStepHeading = () => {
+      const element = createElementWithAttributes('h1', {
+         class: 'welcome-screen__heading',
+         textContent: 'Ultimate Pixel',
+      });
+
+      const span = createElementWithAttributes('span', {
+         class: 'welcome-screen__heading welcome-screen__heading--span',
+         textContent: 'Championships',
+      });
+
+      const line = createElementWithAttributes('span', {
+         class: 'welcome-screen__heading-underline',
+      });
+
+      span.append(line);
+      element.append(span);
+      return element;
+   };
+
    const welcomeStepWrapper = document.createElement('div');
+   welcomeStepWrapper.append(welcomeStepHeading());
    welcomeStepWrapper.append(welcomeStepButtonPrimary());
 
    return welcomeStepWrapper;
