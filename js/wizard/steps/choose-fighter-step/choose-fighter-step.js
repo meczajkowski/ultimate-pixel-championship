3e9af9227459ce05fd0882d80301eaa28b376772
Michal <michal.erik.czajkowski@gmail.com>
2023-08-07 23:38:37 +0200

    feat: create step ui - create chooseFighterStep heading

--- js/wizard/steps/choose-fighter-step/choose-fighter-step.js
+++ js/wizard/steps/choose-fighter-step/choose-fighter-step.js
@@ -1,6 +1,15 @@
+import { createElementWithAttributes } from '../../../helpers/createElementWithAttributes.js';
+
 export const chooseFighterStep = () => {
+   const chooseFighterStepHeading = () => {
+      const element = createElementWithAttributes('h2', {
+         class: 'welcome-screen__heading wizard-heading',
+         textContent: 'Choose your fighter',
+      });
+      return element;
+   };
    const chooseFighterStepWrapper = document.createElement('div');
-   chooseFighterStepWrapper.innerText = 'Choose fighter';
+   chooseFighterStepWrapper.append(chooseFighterStepHeading());
 
    return chooseFighterStepWrapper;
 };
