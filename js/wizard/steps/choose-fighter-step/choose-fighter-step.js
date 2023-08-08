92d4dba795b6949a8f178e7a9d24b685ef21409d
Michal <michal.erik.czajkowski@gmail.com>
2023-08-09 00:12:49 +0200

    feat: create step ui - create chooseFighterStep button

--- js/wizard/steps/choose-fighter-step/choose-fighter-step.js
+++ js/wizard/steps/choose-fighter-step/choose-fighter-step.js
@@ -1,6 +1,7 @@
 import { createElementWithAttributes } from '../../../helpers/createElementWithAttributes.js';
 
 export const chooseFighterStep = () => {
+   // Heading
    const chooseFighterStepHeading = () => {
       const element = createElementWithAttributes('h2', {
          class: 'welcome-screen__heading wizard-heading',
@@ -8,8 +9,21 @@ export const chooseFighterStep = () => {
       });
       return element;
    };
+
+   // Button Primary
+   const chooseFighterStepButtonPrimary = () => {
+      const element = createElementWithAttributes('a', {
+         class: 'button button--primary',
+         textContent: 'Choose',
+         href: '#',
+         onclick: 'route()',
+      });
+      return element;
+   };
+
    const chooseFighterStepWrapper = document.createElement('div');
    chooseFighterStepWrapper.append(chooseFighterStepHeading());
+   chooseFighterStepWrapper.append(chooseFighterStepButtonPrimary());
 
    return chooseFighterStepWrapper;
 };
