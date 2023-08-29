713147d847fee64b6845bd40eb7eaf03383cbd66
Michal <michal.erik.czajkowski@gmail.com>
2023-08-29 22:26:14 +0200

    feat: create booking data step ui - create form

--- js/wizard/steps/booking-details-step/booking-details-step.js
+++ js/wizard/steps/booking-details-step/booking-details-step.js
@@ -1,6 +1,42 @@
+import { createElementWithAttributes } from '../../../helpers/createElementWithAttributes.js';
+
+const form = () => {
+   const form = createElementWithAttributes('form', {
+      class: 'booking-details-form',
+   });
+
+   const input = (type, heading, placeholder) => {
+      const input = createElementWithAttributes('input', {
+         class: `booking-details-form__input booking-details-form__input--${type}`,
+         id: `booking-details-form__input--${type}`,
+         name: heading,
+         placeholder: placeholder,
+      });
+
+      const label = createElementWithAttributes('label', {
+         class: `booking-details-form__label booking-details-form__label--${type}`,
+         for: `booking-details-form__input--${type}`,
+         textContent: heading,
+      });
+
+      const container = createElementWithAttributes('div', {
+         class: 'booking-details-form__input-container',
+      });
+
+      container.append(label, input);
+      return container;
+   };
+
+   form.append(input('text', 'COMMANDER', 'Your name'), input('email', 'EMAIL', 'Your email'));
+   return form;
+};
+
 export const bookingDetailsStep = () => {
-   const bookingDetailsStepWrapper = document.createElement('div');
-   bookingDetailsStepWrapper.innerText = 'Booking details';
+   const bookingDetailsStepWrapper = createElementWithAttributes('div', {
+      class: 'wrapper wrapper--booking-details-step',
+   });
+
+   bookingDetailsStepWrapper.append(form());
 
    return bookingDetailsStepWrapper;
 };
